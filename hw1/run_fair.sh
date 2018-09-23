3482201a1a28e27db71479819bae166eb06dfa73
Tambet Matiisen <tambet.matiisen@gmail.com>
2018-09-23 22:00:54 +0300

    More results.

--- hw1/run_fair.sh
+++ hw1/run_fair.sh
@@ -6,3 +6,10 @@ python bc_fair.py Hopper-v2 $*
 python bc_fair.py Humanoid-v2 $*
 python bc_fair.py Reacher-v2 $*
 python bc_fair.py Walker2d-v2 $*
+
+python dagger.py Ant-v2 $*
+python dagger.py HalfCheetah-v2 $*
+python dagger.py Hopper-v2 $*
+python dagger.py Humanoid-v2 $*
+python dagger.py Reacher-v2 $*
+python dagger.py Walker2d-v2 $*
